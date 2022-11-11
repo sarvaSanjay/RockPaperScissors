af84f34fe0cc52c748f4323847273063e8e50c0d
Sarva Sanjay <sarva.sanjay@mail.utoronto.ca>
2022-11-10 20:55:20 -0500

    GUI built v1

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function GetPlayerChoice(e) {
+    return e.target.getAttribute('id')
+}
+
 function GetComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
     const choice =  Math.floor(Math.random() * 3);
@@ -17,12 +21,63 @@ function playRound(playerChoice, computerChoice) {
     }
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i <5; i++) {
-        let playerChoice = prompt("Choose your weapon: ").toLowerCase();
-        console.log("You chose: " + playerChoice);
+function declareWinner(winner) {
+    winnerBox.setAttribute('style', 'display: flex')
+    declareBox = document.querySelector('.declare-winner');
+    declareBox.textContent = winner + ' won!';
+    buttons.forEach(button => {
+        button.setAttribute('style', 'display: none')
+    })
+
+}
+
+// function game() {
+//     let playerScore = 0;
+//     let computerScore = 0;
+//     while (playerScore < 5 & computerScore < 5) {
+//         let buttons = document.querySelectorAll('button')
+//         buttons.forEach(button => (button.addEventListener('click', function(){let result = GetPlayerChoice()})))
+//         if (result){
+//         playerChoice = result
+//         }
+//         console.log("You chose: " + playerChoice);
+//         let computerChoice = GetComputerChoice();
+//         let result = playRound(playerChoice, computerChoice);
+//         if (result[4] == "L") {
+//             computerScore++;
+//         }
+//         else if (result[4] == "W") {
+//             playerScore++;
+//         }
+//         console.log(result);
+//     }
+//     console.log("Results:");
+//     console.log("Computer Score: " + computerScore);
+//     console.log("Your score: " + playerScore);
+//     if (computerScore > playerScore) {
+//         console.log("You lost")
+//     }
+//     else if (playerScore > computerScore) {
+//         console.log("You won")
+//     }
+//     else {
+//         console.log("Tied")
+//     }
+// }
+
+
+let playerScore = 0;
+let computerScore = 0;
+let buttons = document.querySelectorAll('.choice');
+let winner = document.querySelector('.round');
+let playerBox = document.querySelector('.human-score');
+let computerBox = document.querySelector('.computer-score');
+let winnerBox = document.querySelector('.winner');
+
+buttons.forEach(element => {
+    element.addEventListener('click', (e) => {
+        let playerChoice = e.target.getAttribute('id');
+        console.log(playerChoice);
         let computerChoice = GetComputerChoice();
         let result = playRound(playerChoice, computerChoice);
         if (result[4] == "L") {
@@ -31,26 +86,26 @@ function game() {
         else if (result[4] == "W") {
             playerScore++;
         }
-        console.log(result);
-    }
-    console.log("Results:");
-    console.log("Computer Score: " + computerScore);
-    console.log("Your score: " + playerScore);
-    if (computerScore > playerScore) {
-        console.log("You lost")
-    }
-    else if (playerScore > computerScore) {
-        console.log("You won")
-    }
-    else {
-        console.log("Tied")
-    }
-}
-let quit = false;
-while (!quit) {
-    game();
-    let exit = prompt("Do you want to quit: y/n");
-    if (exit[0] == 'y' | exit[0] == 'Y') {
-        quit = true;
-    }
-}
+        winner.textContent = result;
+        playerBox.textContent = playerScore;
+        computerBox.textContent = computerScore;
+        if (playerScore == 5) {
+            declareWinner('You');
+        }
+        else if (computerScore == 5) {
+            declareWinner('Computer');
+        }
+    })
+});
+
+restart = document.querySelector('#restart');
+restart.addEventListener('click', () => {
+    playerScore = 0;
+    computerScore = 0;
+    playerBox.textContent = playerScore;
+    computerBox.textContent = computerScore;
+    buttons.forEach(button => {
+        button.setAttribute('style', 'display: flex')
+    });
+    winnerBox.setAttribute('style', 'display: none')
+})
